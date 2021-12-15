eafe49bf6ea40286969d2f58aac7d54cc5970626
CraigFoote <craigfoote@gmail.com>
2021-12-14 20:23:54 -0500

    bookings

--- src/main/java/ca/footeware/petclinic/models/BookingDTO.java
+++ src/main/java/ca/footeware/petclinic/models/BookingDTO.java
@@ -12,4 +12,17 @@ public class BookingDTO {
     private Map<String, String> vetIdToNameMap;
     private String procedureName;
     private Map<String, String> procedureIdToNameMap;
+    
+    /**
+     * Constructor displaying bookings.
+     */
+    public BookingDTO(String bookingId, String petId, String vetId, String procedureId, String petName, String vetName, String procedureName){
+        this.bookingId = bookingId;
+        this.petId = petId;
+        this.vetId = vetId;
+        this.procedureId = procedureId;
+        this petNme = petName;
+        this.vetName = vetName;
+        this.procedureName = procedureName;
+    }
 }
